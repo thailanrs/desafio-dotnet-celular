a74285986b40773b6765d994d3f8c3a88a3485c9
Thailan Ramos <thailanrs@gmail.com>
2022-10-08 13:28:26 -0400

    desafio de código DIO - Abstraindo um celular com POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("996959626", "nokia novo", "145616461451", 16);
+Iphone iphone = new Iphone("962151852", "Iphone 11", "65416461451", 64);
+
+nokia.InstalarAplicativo("Amazon Music");
+iphone.InstalarAplicativo("Amazon Music");
